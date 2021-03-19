f467fba13abf4abde34c9c4e438273c4cb7bf3bb
Het <hetp111@users.noreply.github.com>
2021-03-19 19:31:00 +0530

    Contacts Done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,29 @@
-import react from "react";
-import Header from "./Header";
-import Footer from "./Footer";
-import Note from "./Note";
- 
-function App(){
+import React from "react";
+import Card from "./Card";
+import contacts from "../contacts";
+import Avatar from "./Avatar";
+
+let cardList = [];
+
+for (let i = 0; i < contacts.length; i++) {
+    cardList.push(
+        <Card
+            id={contacts[i].id}
+            key={contacts[i].id}//has to be spelled as "key", dont use it yourself
+            name={contacts[i].name}
+            img={contacts[i].imgURL}
+            tel={contacts[i].phone}
+            email={contacts[i].email}
+        />
+    );
+}
+
+function App() {
     return (
         <div>
-        <Header/>
-        <Note/>
-        <Footer/>
+            <h1 className="heading">My Contacts</h1>
+            <Avatar img="https://pbs.twimg.com/profile_images/744849215675838464/IH0FNIXk.jpg" />
+            {cardList}
         </div>
     );
 }
