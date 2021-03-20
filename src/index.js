56f9ebf1aa8954f55dc94176ba8d5cc0132caed7
Het <hetp111@users.noreply.github.com>
2021-03-20 10:49:47 +0530

    Login/Register Conditional rendering - Done

--- src/index.js
+++ src/index.js
@@ -4,5 +4,8 @@ import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
-//Challenge. Render all the notes inside notes.js as a seperate Note
-//component.
+//Challenge: Without moving the userIsRegistered variable,
+//1. Show Login as the button text if userIsRegistered is true.
+//Show Register as the button text if userIsRegistered is false.
+//2. Only show the Confirm Password input if userIsRegistered is false.
+//Don't show it if userIsRegistered is true.
